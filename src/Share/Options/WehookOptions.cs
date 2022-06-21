122649a7ca7eec5dcb563302bd9e1365366b7422
niltor <zpty@outlook.com>
2022-06-21 18:16:06 +0800

    fix options

--- src/Share/Options/WehookOptions.cs
+++ src/Share/Options/WehookOptions.cs
@@ -7,9 +7,16 @@ using System.Threading.Tasks;
 namespace Share.Options
 {
 
-    public record DingTalkOptions(string Name, string NotifyUrl, string Secret);
+    //public record DingTalkOptions(string Name, string NotifyUrl, string Secret);
     public class WebhookOptions
     {
-        public List<DingTalkOptions> DingTalk { get; set; }
+        public List<DingTalkOptions>? DingTalk { get; set; }
+    }
+
+    public class DingTalkOptions
+    {
+        public string? Name { get; set; }
+        public string? NotifyUrl { get; set; }
+        public string? Secret { get; set; }
     }
 }
