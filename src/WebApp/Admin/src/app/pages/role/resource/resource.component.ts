510f14ba90c3fc542acc2cfdc53fb22a1e22eee5
niltor <zpty@outlook.com>
2022-07-10 00:11:19 +0800

    add  role resourcegroup select

--- src/WebApp/Admin/src/app/pages/role/resource/resource.component.ts
+++ src/WebApp/Admin/src/app/pages/role/resource/resource.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { ResourceGroupService } from 'src/app/share/services/resource-group.service';
 import { ResourceGroupRoleDto } from 'src/app/share/models/resource-group/resource-group-role-dto.model';
+import { lastValueFrom } from 'rxjs';
+import { MatSelectionListChange } from '@angular/material/list';
 @Component({
   selector: 'app-resource',
   templateUrl: './resource.component.html',
@@ -12,7 +14,8 @@ export class ResourceComponent implements OnInit {
   id!: string;
   isLoading = true;
   groups = [] as ResourceGroupRoleDto[];
-
+  roleGroups = [] as ResourceGroupRoleDto[];
+  selectedGroupIds: string[] | null = null;
   constructor(
     private route: ActivatedRoute,
     private groupSrv: ResourceGroupService,
@@ -27,7 +30,31 @@ export class ResourceComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getResourceGroups();
+  }
 
+  async getResourceGroups() {
+    let res = await lastValueFrom(this.groupSrv.getRoleResourceGroups(this.id));
+    if (res) {
+      this.roleGroups = res;
+      this.selectedGroupIds = res.map(r => r.id);
+    }
+    res = await lastValueFrom(this.groupSrv.getRoleResourceGroups(''))
+    if (res)
+      this.groups = res;
+
+    this.isLoading = false;
+  }
+
+  /**
+   * 资源组是否在角色中
+   * @param id
+   */
+  inRole(id: string): boolean {
+    return this.roleGroups.find((item) => item.id == id) != undefined;
+  }
+  selectChange(event: MatSelectionListChange): void {
+    this.selectedGroupIds = event.source._value;
   }
   back(): void {
     this.location.back();
