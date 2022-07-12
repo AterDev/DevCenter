282343bf6c783689245c585ddb598e37b885d188
niltor <zpty@outlook.com>
2022-07-12 14:59:58 +0800

    add resource page

--- src/WebApp/Admin/src/app/pages/home/resource/resource.component.ts
+++ src/WebApp/Admin/src/app/pages/home/resource/resource.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { lastValueFrom } from 'rxjs';
+import { NavigationType } from 'src/app/share/models/enum/navigation-type.model';
+import { EnvironmentItemDto } from 'src/app/share/models/environment/environment-item-dto.model';
+import { ResourceGroupFilterDto } from 'src/app/share/models/resource-group/resource-group-filter-dto.model';
+import { ResourceGroupItemDto } from 'src/app/share/models/resource-group/resource-group-item-dto.model';
+import { Resource } from 'src/app/share/models/resource/resource.model';
+import { ResourceGroupService } from 'src/app/share/services/resource-group.service';
 
 @Component({
   selector: 'app-resource',
@@ -6,10 +14,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resource.component.css']
 })
 export class ResourceComponent implements OnInit {
+  isLoading = true;
+  filter = {} as ResourceGroupFilterDto;
+  groups: ResourceGroupItemDto[] = [];
+  environments: EnvironmentItemDto[] = [];
+  constructor(
+    private groupSrv: ResourceGroupService
+  ) {
+    this.filter = {
+      pageIndex: 1,
+      pageSize: 30,
+      navigation: NavigationType.Server,
+      environmentId: null
+    };
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.getResourceGroup();
+  }
+  async getResourceGroup() {
+    let res = await lastValueFrom(this.groupSrv.filter(this.filter));
+    if (res) {
+      this.groups = res.data!;
+      const resources = res.data?.flatMap(g => g.resource!);
+    }
+    this.isLoading = false;
+  }
 }
