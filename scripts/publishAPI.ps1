455e2fc1cb50e3993b83e719a9d03d99ad03c602
niltor <zpty@outlook.com>
2022-07-27 11:09:54 +0800

    publish

--- scripts/publishAPI.ps1
+++ scripts/publishAPI.ps1
@@ -1,3 +1,3 @@
-dotnet publish ..\src\Http.API\Http.API.csproj  -c release -r ubuntu.20.04-x64 --no-self-contained -o ..\publish
+dotnet publish ..\src\Http.API\Http.API.csproj  -c release -o ..\publish
 scp  -r ../publish/*  dev:/var/webapi/dev-center/
 ssh dev sudo systemctl restart dev-center.service
