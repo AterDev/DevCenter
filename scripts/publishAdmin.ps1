dd1099ec277598f9972f671ee60a95ccbead1069
niltor <zpty@outlook.com>
2022-12-07 09:32:54 +0800

    update

--- scripts/publishAdmin.ps1
+++ scripts/publishAdmin.ps1
@@ -1,5 +1,6 @@
 $location = Get-Location
 cd  ../src/WebApp/Admin;
 ng build -c production
-scp  -r ./dist/admin/*  dev:/var/www/dev-center/
+scp  -r ./dist/admin/**/**  dev:/var/www/dev-center/
+scp  -r ./dist/admin/**.*  dev:/var/www/dev-center/
 cd $location
--- src/WebApp/Admin/src/environments/environment.prod.ts
+++ src/WebApp/Admin/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_daemon: 'http://111.40.37.221:14103',
-  uploadEditorFileUrl:'http://111.40.37.221:14103'
+  api_daemon: 'http://111.42.223.4:14103',
+  uploadEditorFileUrl:'http://111.42.223.4:14103'
 };
