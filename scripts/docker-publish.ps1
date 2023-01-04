0c4bf615ee9ec5d3819bbff73bd7b3b075b83dad
niltor <zpty@outlook.com>
2023-01-04 18:32:53 +0800

    update docker

--- scripts/docker-publish.ps1
+++ scripts/docker-publish.ps1
@@ -4,7 +4,7 @@ Set-Location ../
 
 Remove-Item ./publish -Recurse -Force 
 dotnet publish .\src\Http.API\Http.API.csproj -o ./publish -c Release
-docker build  -f  .\src\Http.API\Dockerfile -t dev-center . 
+docker build  -f  .\src\Http.API\Dockerfile -t niltor/dev-center . 
 # docker push dev-center
 
 # docker run  -e ConnectionStrings__Default="Server=192.168.3.32;Port=15432;Database=DevCenter;User Id=postgres;Password=root;" dev-center
