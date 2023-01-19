04c69d93f375d1050dea310b54efc65630e0f6f5
niltor <zpty@outlook.com>
2023-01-19 20:16:53 +0800

    update

--- scripts/docker-publish.ps1
+++ scripts/docker-publish.ps1
@@ -7,7 +7,7 @@ if ([System.IO.Directory]::Exists("./publish")) {
 }
 
 dotnet publish .\src\Http.API\Http.API.csproj -o ./publish -c Release
-docker build  -f  .\src\Http.API\Dockerfile -t niltor/dev-center . 
+docker build  -f  .\src\Http.API\Dockerfile -t niltor/dev-center:latest . 
 # docker push dev-center
 
 # docker run -p 8080:80 -d --name DevCenter -e ConnectionStrings__Default="Server=192.168.0.1;Port=5432;Database=DevCenter;User Id=postgres;Password=root;" niltor/dev-center
